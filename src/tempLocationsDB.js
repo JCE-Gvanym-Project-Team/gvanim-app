01e68c3c9224fe80955fddff98ec0dbfc360a698
aviv0404 <aviv040400@gmail.com>
2023-04-06 18:52:52 +0300

    added a local location database for filtering

---
+++ src/tempLocationsDB.js
@@ -0,0 +1,9 @@
+let locationsDB = [
+    { "Rishon Letzion": [211, 212, 221] },
+    { "Ashdod": [197, 220] },
+    { "Ashkelon": [162, 222, 121] },
+    { "Kiriat Gat": [162, 197, 220] },
+    { "Ofakim": [162] },
+    { "Hasharon": [227] },
+    { "Beer Sheva": [224, 100] }
+];
