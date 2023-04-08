22db52c0d57765099d8159619a6b90d5c03f69c9
aviv0404 <aviv040400@gmail.com>
2023-04-08 20:42:23 +0300

    fixed search text alignment

--- src/Components/Filters/Search.jsx
+++ src/Components/Filters/Search.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { TextField, Container, InputAdornment } from '@mui/material';
+import { TextField, Container } from '@mui/material';
+import { withStyles } from '@mui/styles';
 import searchStyles from "./SearchStyle";
 
 export default function Search({ num_of_jobs }) {
     let placeholder = "חפשו מתוך " + num_of_jobs + " משרות";
     const classes = searchStyles();
+
+    const StyledTextField = withStyles({
+        root: {
+            "& label": {
+                transformOrigin: "top right",
+                right: 28,
+                left: "auto"
+            },
+            "& legend": {
+                textAlign: "right"
+            }
+        }
+    })(TextField);
+
     return (
         <>
             <Container>
-                <TextField
+                <StyledTextField
                     id="search"
                     label={placeholder}
                     variant="outlined"
--- src/Components/Filters/SearchStyle.js
+++ src/Components/Filters/SearchStyle.js
@@ -2,6 +2,13 @@ import { makeStyles } from '@mui/styles'
 import colors from '../../indexStyle'
 
 const searchStyles = makeStyles(theme => ({
+    root: {
+        "& label": {
+            transformOrigin: "top right",
+            right: 0,
+            left: "auto"
+        }
+    },
     searchBar: {
 
     },
