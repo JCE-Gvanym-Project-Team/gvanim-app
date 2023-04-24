7dd0e5476e2275edef00e4b712311b80e80b8306
omer10068 <omer10068@gmail.com>
2023-04-24 14:36:29 +0300

    Added more pages

---
+++ src/DrushimMainPage/DrushimMainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Job from './Components/Job'
+
+export default function DrushimMainPage() {
+  return (
+    <div>
+        <Job description="hi"></Job>
+    </div>
+  )
+}
