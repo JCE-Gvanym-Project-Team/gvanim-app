90f25f5d3dd94d6fa9c13404a2c924ca09015aaa
omer10068 <omer10068@gmail.com>
2023-04-27 13:37:30 +0300

    Add validation to Add/Update Job popup

--- src/RecruiterMainPage/ManageJobsPage/Components/AddOrUpdateJobPopup/AddOrUpdateJobPopup.tsx
+++ src/RecruiterMainPage/ManageJobsPage/Components/AddOrUpdateJobPopup/AddOrUpdateJobPopup.tsx
@@ -14,8 +14,8 @@ type CardProps = {
 }
 
 
-const ModalItem = ({ action, id, data }: CardProps): React.ReactElement => {
-
+const Popup = ({ action, id, data }: CardProps) => {
+  const [validated, setValidated] = useState(false);
   const [show, setShow] = useState(false);
   const [Job_Name, setJobName] = useState('');
   const [Job_Description, setJobDescription] = useState('');
@@ -25,136 +25,165 @@ const ModalItem = ({ action, id, data }: CardProps): React.ReactElement => {
   var newJob = false;
 
   if (action === 'משרה חדשה') {
-    newJob = true;
+      newJob = true;
   }
 
-  const handleClose = () => { setShow(false); }
+  const handleClose = () => {
+      setValidated(false);
+      setShow(false);
+  }
 
   const handleShow = () => {
-    // if i want to edit job
-    if(newJob !== true && data !== null) {
-
-    setJobName(data[id].Job_Name);
-    setJobDescription(data[id].Job_Description);
-    setJobLocation(data[id].Job_Location);
-    setJobPercentage(data[id].Job_Percentage);
-    }
-
-    else {
-      // if i want to create a new job
-
-      setJobName('');
-    setJobDescription('');
-    setJobLocation('');
-    setJobPercentage('');
-    }
-    
-    setShow(true);
-  }
-  
-
-  const handleAdd = () => {
-    if (newJob == true) {
-      // ################ INSERT data ###########
-      dataref.ref("jobs").push().set({
-
-        Job_Name: Job_Name,
-        Job_Description: Job_Description,
-        Job_Location: Job_Location,
-        Job_Percentage: Job_Percentage
-
-      }).catch(alert);
-
-      // ################ END INSERT data ###########
-    }
-    else {
-      // ################ UPDATE data ###########
-      dataref.ref().child(`jobs/${id}`).update({
-
-        Job_Name: Job_Name,
-        Job_Description: Job_Description,
-        Job_Location: Job_Location,
-        Job_Percentage: Job_Percentage
-
-      }).catch(alert);
-      // ################ END UPDATE data ###########
-    }
-    setShow(false);
-
-  }
-
-  return (
-    <>
-      {newJob
-        ? <Button variant='outline-secondary' size='lg' onClick={handleShow} style={{}}>משרה חדשה <LayoutTextSidebar /></Button>
-        : <PencilSquare className="edit" style={{ fontSize: 'medium' }} onClick={handleShow} />
+      // if i want to edit job
+      if (newJob !== true && data !== null) {
+          setJobName(data[id].Job_Name);
+          setJobDescription(data[id].Job_Description);
+          setJobLocation(data[id].Job_Location);
+          setJobPercentage(data[id].Job_Percentage);
       }
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title className="ms-auto">{action}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" dir="rtl">
-              <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>שם המשרה:</Form.Label>
-              <Form.Control
-                type="text"
-                autoFocus
-                value={Job_Name} onChange={(e) => { setJobName(e.target.value) }}
-              />
-            </Form.Group>
+      else {
+          // if i want to create a new job
 
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-              dir="rtl"
-            >
-              <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>תיאור המשרה:</Form.Label>
-              <Form.Control as="textarea" rows={3} value={Job_Description} onChange={(e) => { setJobDescription(e.target.value) }} />
-            </Form.Group>
-          </Form>
+          setJobName('');
+          setJobDescription('');
+          setJobLocation('');
+          setJobPercentage('5%');
+      }
 
-          
-          <div className='parent' dir="rtl">
+      setShow(true);
+  }
 
-            <div className='child1'>
-            <Form.Group className="mb-3">
-              <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}><GeoFill/> איזור:</Form.Label>
 
-              <Form.Control size="sm" type="text" style={{width: '100%'}} placeholder='נתניה'
-              value={Job_Location} onChange={(e) => { setJobLocation(e.target.value) }}
-              />
-            </Form.Group>
-            </div>
+  const handleAdd = () => { 
+      if (newJob == true) {
+          // ################ INSERT data ###########
+          dataref.ref("jobs").push().set({
 
-            <div className='child2'>
-            <Form.Group className="mb-3">
-              <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}><Percent/> אחוז משרה:</Form.Label>
-         
-                <Form.Select size="sm" style={{ width: '100%' }}
-                value={Job_Percentage} onChange={(e) => { setJobPercentage(e.target.value) }}
-                >
-                    <option>5%</option>   <option>10%</option>   <option>15%</option>  <option>20%</option>  <option>25%</option>
-                    <option>30%</option>  <option>35%</option>   <option>40%</option>  <option>45%</option>  <option>50%</option>
-                    <option>55%</option>  <option>60%</option>   <option>65%</option>  <option>70%</option>  <option>75%</option>
-                    <option>80%</option>  <option>85%</option>   <option>90%</option>  <option>95%</option>  <option>100%</option>
-                </Form.Select>
-            </Form.Group>
-            </div>
-          </div>
+              Job_Name: Job_Name,
+              Job_Description: Job_Description,
+              Job_Location: Job_Location,
+              Job_Percentage: Job_Percentage
 
-        </Modal.Body>
-        <Modal.Footer>
-          <div style={{ width: '100%' }}>
-            <Button variant="primary" onClick={handleAdd}>
-              {newJob ? 'פרסם' : 'שמור שינויים'}
-            </Button>
-          </div>
-        </Modal.Footer>
-      </Modal>
-    </>
+          }).catch(alert);
+
+          // ################ END INSERT data ###########
+      }
+      else {
+          // ################ UPDATE data ###########
+          dataref.ref().child(`jobs/${id}`).update({
+
+              Job_Name: Job_Name,
+              Job_Description: Job_Description,
+              Job_Location: Job_Location,
+              Job_Percentage: Job_Percentage
+
+          }).catch(alert);
+          // ################ END UPDATE data ###########
+      }
+      setShow(false);
+
+  }
+
+  const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+      }
+      else {
+          handleAdd();
+
+      }
+
+      setValidated(true);
+
+  };
+
+  return (
+
+      <>
+          {newJob
+              ? <Button variant='outline-secondary' size='lg' onClick={handleShow} style={{}}>משרה חדשה <LayoutTextSidebar /></Button>
+              : <PencilSquare className="edit" style={{ fontSize: 'medium' }} onClick={handleShow} />
+          }
+          <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                  <Modal.Title className="ms-auto">{action}</Modal.Title>
+              </Modal.Header>
+              <Form noValidate validated={validated} onSubmit={handleSubmit} dir='rtl'>
+                  <Modal.Body>
+
+                      <Form>
+                          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" dir="rtl">
+                              <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>שם המשרה:</Form.Label>
+                              <Form.Control
+                                  required
+                                  type="text"
+                                  autoFocus
+                                  value={Job_Name} onChange={(e) => { setJobName(e.target.value) }}
+                              />
+                          </Form.Group>
+
+                          <Form.Group
+                              className="mb-3"
+                              controlId="exampleForm.ControlTextarea1"
+                              dir="rtl"
+                          >
+                              <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>תיאור המשרה:</Form.Label>
+                              <Form.Control
+                                  required
+                                  as="textarea"
+                                  rows={3} value={Job_Description} onChange={(e) => { setJobDescription(e.target.value) }} />
+                          </Form.Group>
+                      </Form>
+
+
+                      <div className='parent' dir="rtl">
+
+                          <div className='child1'>
+                              <Form.Group className="mb-3">
+                                  <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}><GeoFill /> איזור:</Form.Label>
+
+                                  <Form.Control
+                                      size="sm"
+                                      required
+                                      type="text"
+                                      placeholder='נתניה'
+                                      value={Job_Location} onChange={(e) => { setJobLocation(e.target.value) }}
+                                  />
+                              </Form.Group>
+                          </div>
+
+                          <div className='child2'>
+                              <Form.Group className="mb-3">
+                                  <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}><Percent /> אחוז משרה:</Form.Label>
+
+                                  <Form.Select
+                                      required size="sm"
+                                      value={Job_Percentage} onChange={(e) => { setJobPercentage(e.target.value) }}
+                                  >
+                                      <option>5%</option>   <option>10%</option>   <option>15%</option>  <option>20%</option>  <option>25%</option>
+                                      <option>30%</option>  <option>35%</option>   <option>40%</option>  <option>45%</option>  <option>50%</option>
+                                      <option>55%</option>  <option>60%</option>   <option>65%</option>  <option>70%</option>  <option>75%</option>
+                                      <option>80%</option>  <option>85%</option>   <option>90%</option>  <option>95%</option>  <option>100%</option>
+                                  </Form.Select>
+                              </Form.Group>
+                          </div>
+                      </div>
+
+                  </Modal.Body>
+                  <Modal.Footer>
+
+                      <Button variant="primary" type='submit'>
+                          {newJob ? 'פרסם' : 'שמור שינויים'}
+                      </Button>
+
+                  </Modal.Footer>
+              </Form>
+          </Modal>
+      </>
   );
 }
 
-export default ModalItem;
+export default Popup;
