3f08b6d2bab28dab9ea02afab9d8ac5852618fd4
Gavriel <Gavrish299@gmail.com>
2023-05-03 00:42:17 +0300

    NavBar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,7 +1,105 @@
-import React from 'react'
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import ManageCandidatesPage from "./../../RecruiterMainPage/ManageCandidatesPage/ManageCandidatesPage";
+import ReportsPage from "./../../RecruiterMainPage/ReportsPage/ReportsPage";
+import RecruiterMainPage from "./../../RecruiterMainPage/RecruiterMainPage";
+import "./NavBar.css";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-bootstrap-icons";
+import ManageJobsPage from "../../RecruiterMainPage/ManageJobsPage/ManageJobsPage";
 
-export default function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+class NavBar extends Component {
+  state = {
+    activePage: "recruiterMainPage",
+  };
+
+  handleClick = (page) => {
+    this.setState({ activePage: page });
+  };
+
+  render() {
+    const { activePage } = this.state;
+
+    return (
+      <Router>
+        <React.Fragment>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-light-blue">
+            <Link className="navbar-brand" to="/">
+              Navbar
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="navbar-collapse collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <NavLink
+                  className={`nav-item nav-link ${
+                    activePage === "recruiterMainPage" ? "active" : ""
+                  }`}
+                  to="/recruiterMainPage"
+                  onClick={() => this.handleClick("recruiterMainPage")}
+                >
+                  ראשי
+                </NavLink>
+                <NavLink
+                  className={`nav-item nav-link ${
+                    activePage === "manageCandidates" ? "active" : ""
+                  }`}
+                  to="/manageCandidates"
+                  onClick={() => this.handleClick("manageCandidates")}
+                >
+                  ניהול מועמדים
+                </NavLink>
+                <NavLink
+                  className={`nav-item nav-link ${
+                    activePage === "manageJobs" ? "active" : ""
+                  }`}
+                  to="/manageJobs"
+                  onClick={() => this.handleClick("manageJobs")}
+                >
+                  ניהול משרות
+                </NavLink>
+                <NavLink
+                  className={`nav-item nav-link ${
+                    activePage === "reports" ? "active" : ""
+                  }`}
+                  to="/reports"
+                  onClick={() => this.handleClick("reports")}
+                >
+                  דו"ח
+                </NavLink>
+              </div>
+            </div>
+          </nav>
+          <main className="container">
+            <Switch>
+              <Route path="/recruiterMainPage" component={RecruiterMainPage} />
+              <Route path="/manageJobs" component={ManageJobsPage} />
+              <Route
+                path="/manageCandidates"
+                component={ManageCandidatesPage}
+              />
+              <Route path="/reports" component={ReportsPage} />
+              <Redirect from="/" to="/recruiterMainPage" />
+            </Switch>
+          </main>
+        </React.Fragment>
+      </Router>
+    );
+  }
 }
+
+export default NavBar;
--- src/RecruiterMainPage/RecruiterMainPage.tsx
+++ src/RecruiterMainPage/RecruiterMainPage.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ManageCandidatesPage from './ManageCandidatesPage/ManageCandidatesPage'
-import ManageJobsPage from './ManageJobsPage/ManageJobsPage'
-import ReportsPage from './ReportsPage/ReportsPage'
+import React from "react";
+import ManageCandidatesPage from "./ManageCandidatesPage/ManageCandidatesPage";
+import ManageJobsPage from "./ManageJobsPage/ManageJobsPage";
+import ReportsPage from "./ReportsPage/ReportsPage";
 
 export default function RecruiterMainPage() {
   return (
- <>
-   <ManageCandidatesPage />
-   <ManageJobsPage />
-   <ReportsPage />
- </>
-  )
+    <>
+      <ManageCandidatesPage />
+      <ManageJobsPage />
+      <ReportsPage />
+    </>
+  );
 }
