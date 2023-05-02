fb6ad1cc85623a458d43cf5639b32658885a723e
Gavriel <Gavrish299@gmail.com>
2023-05-02 19:53:57 +0300

    landing page

--- src/RecruiterMainPage/RecruiterMainPage.tsx
+++ src/RecruiterMainPage/RecruiterMainPage.tsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import ManageCandidatesPage from './ManageCandidatesPage/ManageCandidatesPage'
-import ManageJobsPage from './ManageJobsPage/ManageJobsPage'
-import ReportsPage from './ReportsPage/ReportsPage'
+import React, { useState, useEffect } from "react";
+import "./RecruiterMainPage.css";
+import { dataref } from "../FirebaseConfig/firebase";
+import ManageCandidatesPage from "./ManageCandidatesPage/ManageCandidatesPage";
+import ManageJobsPage from "./ManageJobsPage/ManageJobsPage";
+import ReportsPage from "./ReportsPage/ReportsPage";
+import Buttons1 from "./Components/Buttons";
+import Circles from "./Components/Circles";
+import { Route, Link } from "react-router-dom";
+import { redirect as Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 
-export default function RecruiterMainPage() {
+function Buttons() {
   return (
- <>
-   <ManageCandidatesPage />
-   <ManageJobsPage />
-   <ReportsPage />
- </>
-  )
+    <div className="button-group">
+      <Link to="/ManageCandidatesPage">
+        <button className="button">ניהול מועמדים</button>
+      </Link>
+      <Link to="/ManageJobsPage">
+        <button className="button">ניהול משרות </button>
+      </Link>
+      <Link to="/ReportsPage">
+        <button className="button">דו"ח</button>
+      </Link>
+    </div>
+  );
 }
+
+function RecruiterMainPage() {
+  return (
+    <Router>
+      <div className="recruiter-main-page">
+        <Buttons />
+        <Circles />
+        <Routes>
+          <Route
+            path="./ManageCandidatesPage/ManageCandidatesPage"
+            Component={ManageCandidatesPage}
+          />
+          <Route
+            path="./ManageJobsPage/ManageJobsPage"
+            Component={ManageJobsPage}
+          />
+          <Route path="/ReportsPage" Component={ReportsPage} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default RecruiterMainPage;
