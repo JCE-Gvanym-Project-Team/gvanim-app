e107450869c081543eba5d93ca89ae91ca2797e6
Gavriel <Gavrish299@gmail.com>
2023-05-02 20:28:23 +0300

    done for now

--- src/RecruiterMainPage/Components/Buttons.tsx
+++ src/RecruiterMainPage/Components/Buttons.tsx
@@ -4,20 +4,20 @@ import ManageCandidatesPage from "../ManageCandidatesPage/ManageCandidatesPage";
 import ManageJobsPage from "../ManageJobsPage/ManageJobsPage";
 import ReportsPage from "../ReportsPage/ReportsPage";
 
-function Buttons1() {
+function Buttons() {
   return (
     <div className="button-group">
-      <Link to="ManageCandidatesPage">
-        <button className="button">Manage Candidates</button>
+      <Link to="/ManageCandidatesPage">
+        <button className="button">ניהול מועמדים</button>
       </Link>
-      <Link to="ManageJobsPage">
-        <button className="button">Job Management</button>
+      <Link to="/ManageJobsPage">
+        <button className="button">ניהול משרות </button>
       </Link>
-      <Link to="ReportsPage">
-        <button className="button">Report</button>
+      <Link to="/ReportsPage">
+        <button className="button">דו"ח</button>
       </Link>
     </div>
   );
 }
 
-export default Buttons1;
+export default Buttons;
--- src/RecruiterMainPage/RecruiterMainPage.tsx
+++ src/RecruiterMainPage/RecruiterMainPage.tsx
@@ -4,27 +4,11 @@ import { dataref } from "../FirebaseConfig/firebase";
 import ManageCandidatesPage from "./ManageCandidatesPage/ManageCandidatesPage";
 import ManageJobsPage from "./ManageJobsPage/ManageJobsPage";
 import ReportsPage from "./ReportsPage/ReportsPage";
-import Buttons1 from "./Components/Buttons";
+import Buttons from "./Components/Buttons";
 import Circles from "./Components/Circles";
 import { Route, Link } from "react-router-dom";
 import { redirect as Redirect } from "react-router-dom";
 import { BrowserRouter as Router, Routes } from "react-router-dom";
-
-function Buttons() {
-  return (
-    <div className="button-group">
-      <Link to="/ManageCandidatesPage">
-        <button className="button">ניהול מועמדים</button>
-      </Link>
-      <Link to="/ManageJobsPage">
-        <button className="button">ניהול משרות </button>
-      </Link>
-      <Link to="/ReportsPage">
-        <button className="button">דו"ח</button>
-      </Link>
-    </div>
-  );
-}
 
 function RecruiterMainPage() {
   return (
