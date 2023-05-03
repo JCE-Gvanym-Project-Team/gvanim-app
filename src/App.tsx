f886c791f854efb4cb776a2c51cba32e3c8f3cb5
aviv zvuluny <aviv040400@gmail.com>
2023-05-03 13:48:46 +0300

    fixed deploy in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import DrushimMainPage from './DrushimMainPage/DrushimMainPage';
 import AdminMainPage from './AdminMainPage/AdminMainPage';
 import RecruiterMainPage from './RecruiterMainPage/RecruiterMainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Route} from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import ManageCandidatesPage from './RecruiterMainPage/ManageCandidatesPage/ManageCandidatesPage';
 import ManageJobsPage from './RecruiterMainPage/ManageJobsPage/ManageJobsPage';
 import ReportsPage from './RecruiterMainPage/ReportsPage/ReportsPage';
@@ -14,34 +14,34 @@ const Admin = "admin";
 const Recruiter = "recruiter";
 
 function App() {
-  const currentUser = "recruiter";
-  return (
-    <>
-	<NavBar />
-    <BrowserRouter>
-		<div>
-			<Route exact path="/" component={RecruiterMainPage} />
-			<Route path="/manageCandidates" component={ManageCandidatesPage} />
-			<Route path="/manageJobs" component={ManageJobsPage} />
-			<Route path="/reports" component={ReportsPage} />
-		</div>
-    </BrowserRouter>
-    </>
-  );
+	const currentUser = "recruiter";
+	return (
+		<>
+			<NavBar />
+			<BrowserRouter>
+				<div>
+					<Route exact path="/" component={RecruiterMainPage} />
+					<Route path="/manageCandidates" component={ManageCandidatesPage} />
+					<Route path="/manageJobs" component={ManageJobsPage} />
+					<Route path="/reports" component={ReportsPage} />
+				</div>
+			</BrowserRouter>
+		</>
+	);
 }
 
-function decidePage(currentUser: any){
-  if (currentUser === Admin){
-    return (<AdminMainPage />)
-  }else if (currentUser === Recruiter){
-    return (
-      <RecruiterMainPage />
-    )
-  }else{
-    return (
-      <DrushimMainPage />
-    )
-  }
+function decidePage(currentUser: any) {
+	if (currentUser === Admin) {
+		return (<AdminMainPage />)
+	} else if (currentUser === Recruiter) {
+		return (
+			<RecruiterMainPage />
+		)
+	} else {
+		return (
+			<DrushimMainPage />
+		)
+	}
 }
 
 export default App;
