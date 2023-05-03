d37ec2f3709b2f9cdeb10ac64780139c8b9055b4
aviv zvuluny <aviv040400@gmail.com>
2023-05-03 15:31:26 +0300

    added comments to talk about tomorrow

--- src/RecruiterMainPage/RecruiterMainPage.tsx
+++ src/RecruiterMainPage/RecruiterMainPage.tsx
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./RecruiterMainPage.css";
-import { dataref } from "../FirebaseConfig/firebase";
-import ManageCandidatesPage from "./ManageCandidatesPage/ManageCandidatesPage";
-import ManageJobsPage from "./ManageJobsPage/ManageJobsPage";
-import ReportsPage from "./ReportsPage/ReportsPage";
 import Buttons from "./Components/Buttons";
 import Circles from "./Components/Circles";
-import { Route, Link } from "react-router-dom";
-import NavBar from "../Components/NavBar/NavBar";
 
 function RecruiterMainPage() {
   return (
