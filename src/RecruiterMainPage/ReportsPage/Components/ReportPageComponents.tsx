7ecc340f2047f63d81198fc53a76e4ad53a96055
Eliyahu-Yishai <eli089743@gmail.com>
2023-05-03 12:35:29 +0300

    add search bar and sort

--- src/RecruiterMainPage/ReportsPage/Components/ReportPageComponents.tsx
+++ src/RecruiterMainPage/ReportsPage/Components/ReportPageComponents.tsx
@@ -1,10 +1,11 @@
 import { type } from '@testing-library/user-event/dist/type'
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import { Container, Form, Button, InputGroup } from "react-bootstrap";
 import { GeoFill, LayoutTextSidebar, PencilSquare, Percent } from 'react-bootstrap-icons';
 import { Component } from 'react';
+import { Search } from "react-bootstrap-icons";
+
 
 interface Props {}
 
@@ -12,9 +13,53 @@ interface State {}
 
 
 const HistoryTable = () => {
+    const [search, setSearch] = useState('');
+
     return (
      <div>
-      <button style={{}} id="createReport" >צור דו"ח חדש <LayoutTextSidebar /></button>
+      {/* button */}
+      <button style={{}} id="createReport" >צור דו"ח חדש<LayoutTextSidebar /></button>
+      {/* ######### sort ############### */}
+      <Form.Group className="d-flex flex-column bd-highlight mb-3" dir="rtl" style={{ flex: '1', margin: '5px'}}>
+            <div className="d-flex align-items-end" style={{ height: '100%' }}>
+                <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>מיין לפי:</Form.Label>
+            </div>
+            <div className="d-flex align-items-end" style={{ height: '100%' }}>
+                <Form.Select
+                    size="sm"
+                    style={{ width: '100%' }}
+                    defaultChecked={true}
+                    defaultValue={'ללא'}
+                    // onChange={(e) => { setSortOrder(e.target.value) }}
+                     >
+                    <option>ללא</option>
+                    <option>מס' משרה</option>
+                    <option>איזור</option>
+                    <option>תפקיד</option>
+                    <option>אחוז משרה</option>
+                </Form.Select>
+            </div>
+        </Form.Group>
+        {/* ########### end sort ########### */}
+         
+         {/* ############## search bar ############ */}
+         <Form.Group className="d-flex flex-column bd-highlight mb-3" style={{ flex: '1', margin: '5px'}}>
+          <div className="d-flex align-items-end" style={{ height: '100%' }}>
+          <InputGroup size="sm">
+            <Form.Control
+                onChange={(e) => { setSearch(e.target.value) }}
+                dir="rtl"
+                placeholder="חיפוש"
+                aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-primary">
+                <Search />
+              </Button>
+            </InputGroup>
+              </div>
+          </Form.Group>
+
+
       <table id="historyReports">
         <thead>
           <tr>
