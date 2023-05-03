ed22ef1921de1977d2c186652c565dcf6517b887
omer10068 <omer10068@gmail.com>
2023-05-03 04:01:44 +0300

    design login & import svg logo

--- src/RecruiterMainPage/ManageJobsPage/Components/AddOrUpdateJobPopup/AddOrUpdateJobPopup.tsx
+++ src/RecruiterMainPage/ManageJobsPage/Components/AddOrUpdateJobPopup/AddOrUpdateJobPopup.tsx
@@ -8,182 +8,182 @@ import { GeoFill, LayoutTextSidebar, PencilSquare, Percent } from 'react-bootstr
 
 
 type CardProps = {
-    action: string,
-    id: string,
-    data: {}
+  action: string,
+  id: string,
+  data: {}
 }
 
 
 const Popup = ({ action, id, data }: CardProps) => {
-    const [validated, setValidated] = useState(false);
-    const [show, setShow] = useState(false);
-    const [Job_Name, setJobName] = useState('');
-    const [Job_Description, setJobDescription] = useState('');
-    const [Job_Location, setJobLocation] = useState('');
-    const [Job_Percentage, setJobPercentage] = useState('');
+  const [validated, setValidated] = useState(false);
+  const [show, setShow] = useState(false);
+  const [Job_Name, setJobName] = useState('');
+  const [Job_Description, setJobDescription] = useState('');
+  const [Job_Location, setJobLocation] = useState('');
+  const [Job_Percentage, setJobPercentage] = useState('');
 
-    var newJob = false;
+  var newJob = false;
 
-    if (action === 'משרה חדשה') {
-        newJob = true;
-    }
+  if (action === 'משרה חדשה') {
+      newJob = true;
+  }
 
-    const handleClose = () => {
-        setValidated(false);
-        setShow(false);
-    }
+  const handleClose = () => {
+      setValidated(false);
+      setShow(false);
+  }
 
-    const handleShow = () => {
-        // if i want to edit job
-        if (newJob !== true && data !== null) {
-            setJobName(data[id].Job_Name);
-            setJobDescription(data[id].Job_Description);
-            setJobLocation(data[id].Job_Location);
-            setJobPercentage(data[id].Job_Percentage);
-        }
+  const handleShow = () => {
+      // if i want to edit job
+      if (newJob !== true && data !== null) {
+          setJobName(data[id].Job_Name);
+          setJobDescription(data[id].Job_Description);
+          setJobLocation(data[id].Job_Location);
+          setJobPercentage(data[id].Job_Percentage);
+      }
 
-        else {
-            // if i want to create a new job
+      else {
+          // if i want to create a new job
 
-            setJobName('');
-            setJobDescription('');
-            setJobLocation('');
-            setJobPercentage('5%');
-        }
+          setJobName('');
+          setJobDescription('');
+          setJobLocation('');
+          setJobPercentage('5%');
+      }
 
-        setShow(true);
-    }
-
-
-    const handleAdd = () => { 
-        if (newJob == true) {
-            // ################ INSERT data ###########
-            dataref.ref("jobs").push().set({
-
-                Job_Name: Job_Name,
-                Job_Description: Job_Description,
-                Job_Location: Job_Location,
-                Job_Percentage: Job_Percentage
-
-            }).catch(alert);
-
-            // ################ END INSERT data ###########
-        }
-        else {
-            // ################ UPDATE data ###########
-            dataref.ref().child(`jobs/${id}`).update({
-
-                Job_Name: Job_Name,
-                Job_Description: Job_Description,
-                Job_Location: Job_Location,
-                Job_Percentage: Job_Percentage
-
-            }).catch(alert);
-            // ################ END UPDATE data ###########
-        }
-        setShow(false);
-
-    }
-
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        else {
-            handleAdd();
-
-        }
-
-        setValidated(true);
-
-    };
-
-    return (
-
-        <>
-            {newJob
-                ? <Button variant='outline-secondary' onClick={handleShow} style={{}}>משרה חדשה <LayoutTextSidebar /></Button>
-                : <PencilSquare className="edit" style={{ fontSize: 'medium' }} onClick={handleShow} />
-            }
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title className="ms-auto">{action}</Modal.Title>
-                </Modal.Header>
-                <Form noValidate validated={validated} onSubmit={handleSubmit} dir='rtl'>
-                    <Modal.Body>
-
-                        <Form>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" dir="rtl">
-                                <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>שם המשרה:</Form.Label>
-                                <Form.Control
-                                    required
-                                    type="text"
-                                    autoFocus
-                                    value={Job_Name} onChange={(e) => { setJobName(e.target.value) }}
-                                />
-                            </Form.Group>
-
-                            <Form.Group
-                                className="mb-3"
-                                controlId="exampleForm.ControlTextarea1"
-                                dir="rtl"
-                            >
-                                <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>תיאור המשרה:</Form.Label>
-                                <Form.Control
-                                    required
-                                    as="textarea"
-                                    rows={3} value={Job_Description} onChange={(e) => { setJobDescription(e.target.value) }} />
-                            </Form.Group>
-                        </Form>
+      setShow(true);
+  }
 
 
-                        <div className='parent' dir="rtl">
+  const handleAdd = () => { 
+      if (newJob == true) {
+          // ################ INSERT data ###########
+          dataref.ref("jobs").push().set({
 
-                            <div className='child1'>
-                                <Form.Group className="mb-3">
-                                    <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}><GeoFill /> איזור:</Form.Label>
+              Job_Name: Job_Name,
+              Job_Description: Job_Description,
+              Job_Location: Job_Location,
+              Job_Percentage: Job_Percentage
 
-                                    <Form.Control
-                                        size="sm"
-                                        required
-                                        type="text"
-                                        placeholder='נתניה'
-                                        value={Job_Location} onChange={(e) => { setJobLocation(e.target.value) }}
-                                    />
-                                </Form.Group>
-                            </div>
+          }).catch(alert);
 
-                            <div className='child2'>
-                                <Form.Group className="mb-3">
-                                    <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}><Percent /> אחוז משרה:</Form.Label>
+          // ################ END INSERT data ###########
+      }
+      else {
+          // ################ UPDATE data ###########
+          dataref.ref().child(`jobs/${id}`).update({
 
-                                    <Form.Select
-                                        required size="sm"
-                                        value={Job_Percentage} onChange={(e) => { setJobPercentage(e.target.value) }}
-                                    >
-                                        <option>5%</option>   <option>10%</option>   <option>15%</option>  <option>20%</option>  <option>25%</option>
-                                        <option>30%</option>  <option>35%</option>   <option>40%</option>  <option>45%</option>  <option>50%</option>
-                                        <option>55%</option>  <option>60%</option>   <option>65%</option>  <option>70%</option>  <option>75%</option>
-                                        <option>80%</option>  <option>85%</option>   <option>90%</option>  <option>95%</option>  <option>100%</option>
-                                    </Form.Select>
-                                </Form.Group>
-                            </div>
-                        </div>
+              Job_Name: Job_Name,
+              Job_Description: Job_Description,
+              Job_Location: Job_Location,
+              Job_Percentage: Job_Percentage
 
-                    </Modal.Body>
-                    <Modal.Footer>
+          }).catch(alert);
+          // ################ END UPDATE data ###########
+      }
+      setShow(false);
 
-                        <Button variant="primary" type='submit'>
-                            {newJob ? 'פרסם' : 'שמור שינויים'}
-                        </Button>
+  }
 
-                    </Modal.Footer>
-                </Form>
-            </Modal>
-        </>
-    );
+  const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+      }
+      else {
+          handleAdd();
+
+      }
+
+      setValidated(true);
+
+  };
+
+  return (
+
+      <>
+          {newJob
+              ? <Button variant='outline-secondary' size='lg' onClick={handleShow} style={{}}>משרה חדשה <LayoutTextSidebar /></Button>
+              : <PencilSquare className="edit" style={{ fontSize: 'medium' }} onClick={handleShow} />
+          }
+          <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                  <Modal.Title className="ms-auto">{action}</Modal.Title>
+              </Modal.Header>
+              <Form noValidate validated={validated} onSubmit={handleSubmit} dir='rtl'>
+                  <Modal.Body>
+
+                      <Form>
+                          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" dir="rtl">
+                              <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>שם המשרה:</Form.Label>
+                              <Form.Control
+                                  required
+                                  type="text"
+                                  autoFocus
+                                  value={Job_Name} onChange={(e) => { setJobName(e.target.value) }}
+                              />
+                          </Form.Group>
+
+                          <Form.Group
+                              className="mb-3"
+                              controlId="exampleForm.ControlTextarea1"
+                              dir="rtl"
+                          >
+                              <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>תיאור המשרה:</Form.Label>
+                              <Form.Control
+                                  required
+                                  as="textarea"
+                                  rows={3} value={Job_Description} onChange={(e) => { setJobDescription(e.target.value) }} />
+                          </Form.Group>
+                      </Form>
+
+
+                      <div className='parent' dir="rtl">
+
+                          <div className='child1'>
+                              <Form.Group className="mb-3">
+                                  <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}><GeoFill /> איזור:</Form.Label>
+
+                                  <Form.Control
+                                      size="sm"
+                                      required
+                                      type="text"
+                                      placeholder='נתניה'
+                                      value={Job_Location} onChange={(e) => { setJobLocation(e.target.value) }}
+                                  />
+                              </Form.Group>
+                          </div>
+
+                          <div className='child2'>
+                              <Form.Group className="mb-3">
+                                  <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}><Percent /> אחוז משרה:</Form.Label>
+
+                                  <Form.Select
+                                      required size="sm"
+                                      value={Job_Percentage} onChange={(e) => { setJobPercentage(e.target.value) }}
+                                  >
+                                      <option>5%</option>   <option>10%</option>   <option>15%</option>  <option>20%</option>  <option>25%</option>
+                                      <option>30%</option>  <option>35%</option>   <option>40%</option>  <option>45%</option>  <option>50%</option>
+                                      <option>55%</option>  <option>60%</option>   <option>65%</option>  <option>70%</option>  <option>75%</option>
+                                      <option>80%</option>  <option>85%</option>   <option>90%</option>  <option>95%</option>  <option>100%</option>
+                                  </Form.Select>
+                              </Form.Group>
+                          </div>
+                      </div>
+
+                  </Modal.Body>
+                  <Modal.Footer>
+
+                      <Button variant="primary" type='submit'>
+                          {newJob ? 'פרסם' : 'שמור שינויים'}
+                      </Button>
+
+                  </Modal.Footer>
+              </Form>
+          </Modal>
+      </>
+  );
 }
 
 export default Popup;
--- src/RecruiterMainPage/ManageJobsPage/ManageJobsPage.tsx
+++ src/RecruiterMainPage/ManageJobsPage/ManageJobsPage.tsx
@@ -14,152 +14,142 @@ const TableContainer = () => {
   const [sortOrder, setSortOrder] = useState('');
 
   useEffect(() => {
-      dataref.ref().child('jobs').on('value', (snapshot) => {
-          if (snapshot.val() !== null) {
-              setData({ ...snapshot.val() });
-          }
-          else {
-              setData({});
-          }
-      });
+    dataref.ref().child('jobs').on('value', (snapshot) => {
+      if (snapshot.val() !== null) {
+        setData({ ...snapshot.val() });
+      }
+      else {
+        setData({});
+      }
+    });
 
-      return () => {
-          setData({});
-      };
+    return () => {
+      setData({});
+    };
   }, []);
 
 
   return (
-      <>
+    <>
 
-          <Container className="mt-5">
-              <div className="d-flex flex-row bd-highlight" style={{marginLeft: '10px'}}>
-                  <div className="d-flex justify-content-start">
-                      <AddOrUpdateJobPopup action={"משרה חדשה"} id="" data={{}} />
-                  </div>
-              </div>
+      <Container className="mt-5">
 
-              <div style={{ display: 'flex', margin: '5px'}}>
-                  {/* ####################################### SEARCH BAR ################################################################ */}
-                  <Form.Group className="d-flex flex-column bd-highlight mb-3" style={{ flex: '1', margin: '5px'}}>
-                      <div className="d-flex align-items-end" style={{ height: '100%' }}>
-                          <InputGroup size="sm">
-                              <Form.Control
-                                  onChange={(e) => { setSearch(e.target.value) }}
-                                  dir="rtl"
-                                  placeholder="חיפוש"
-                                  aria-describedby="basic-addon2"
-                              />
-                              <Button variant="outline-primary">
-                                  <Search />
-                              </Button>
-                          </InputGroup>
-                      </div>
-                  </Form.Group>
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: '1', display: 'flex', alignItems: 'center' }}>
 
-                  {/* ####################################### END SEARCH BAR ############################################################ */}
+            <AddOrUpdateJobPopup action="משרה חדשה" id="" data={{}} />
+          </div>
+          {/* ####################################### SEARCH BAR ################################################################ */}
+          <Form.Group style={{ flex: '1', margin: '5px' }}>
+            <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold', visibility: 'hidden' }}>מיין לפי:</Form.Label>
+            <InputGroup className="mb-3" size="sm">
+              <Form.Control
+                onChange={(e) => { setSearch(e.target.value) }}
+                dir="rtl"
+                placeholder="חיפוש"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="outline-primary">
+                <Search />
+              </Button>
+            </InputGroup>
+          </Form.Group>
+          {/* ####################################### END SEARCH BAR ############################################################ */}
 
-                  {/* ####################################### FILTER ############################################################ */}
-                  <Form.Group className="d-flex flex-column bd-highlight mb-3" dir="rtl" style={{ flex: '1', margin: '5px'}}>
-                      <div className="d-flex align-items-end" style={{ height: '100%' }}>
-                          <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>סנן לפי:</Form.Label>
-                      </div>
+          {/* ####################################### FILTER ############################################################ */}
+          <Form.Group className="mb-3" dir="rtl" style={{ flex: '1', margin: '5px' }}>
+            <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>סנן לפי:</Form.Label>
 
-                      <div className="d-flex align-items-end" style={{ height: '100%' }}>
-                          <Form.Select
-                              size="sm"
-                              defaultChecked={true}
-                              defaultValue={'ללא'}
-                              onChange={(e) => { setFilter(e.target.value) }}
-                          >
-                              <option>ללא</option>
-                              <option>מס' משרה</option>
-                              <option>איזור</option>
-                              <option>תפקיד</option>
-                              <option>אחוז משרה</option>
-                          </Form.Select>
-                      </div>
+            <Form.Select
+              size="sm"
+              style={{ width: '100%' }}
+              defaultChecked={true}
+              defaultValue={'ללא'}
+              onChange={(e) => { setFilter(e.target.value) }}
+            >
+              <option>ללא</option>
+              <option>מס' משרה</option>
+              <option>איזור</option>
+              <option>תפקיד</option>
+              <option>אחוז משרה</option>
+            </Form.Select>
+          </Form.Group>
+          {/* ####################################### END FILTER ######################################################### */}
 
-                  </Form.Group>
+          {/* ####################################### SORT ############################################################ */}
+          <Form.Group className="mb-3" dir="rtl" style={{ flex: '1', margin: '5px' }}>
+            <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>מיין לפי:</Form.Label>
 
-                  {/* ####################################### END FILTER ######################################################### */}
+            <Form.Select
+              size="sm"
+              style={{ width: '100%' }}
+              defaultChecked={true}
+              defaultValue={'ללא'}
+              onChange={(e) => { setSortOrder(e.target.value) }}
+            >
+              <option>ללא</option>
+              <option>מס' משרה</option>
+              <option>איזור</option>
+              <option>תפקיד</option>
+              <option>אחוז משרה</option>
+            </Form.Select>
+          </Form.Group>
+          {/* ####################################### END SORT ######################################################### */}
 
-                  {/* ####################################### SORT ############################################################ */}
-                  <Form.Group className="d-flex flex-column bd-highlight mb-3" dir="rtl" style={{ flex: '1', margin: '5px'}}>
-                      <div className="d-flex align-items-end" style={{ height: '100%' }}>
-                          <Form.Label style={{ fontSize: 'smaller', fontWeight: 'bold' }}>מיין לפי:</Form.Label>
-                      </div>
-
-                      <div className="d-flex align-items-end" style={{ height: '100%' }}>
-                          <Form.Select
-                              size="sm"
-                              style={{ width: '100%' }}
-                              defaultChecked={true}
-                              defaultValue={'ללא'}
-                              onChange={(e) => { setSortOrder(e.target.value) }}
-                          >
-                              <option>ללא</option>
-                              <option>מס' משרה</option>
-                              <option>איזור</option>
-                              <option>תפקיד</option>
-                              <option>אחוז משרה</option>
-                          </Form.Select>
-                      </div>
-                  </Form.Group>
-                  {/* ####################################### END SORT ######################################################### */}
-
-              </div>
-
-              <table dir='rtl' style={{ width: '100%'}}>
-                  <thead>
-                      <tr>
-                          <th></th>
-                          <th><LayoutTextSidebar /> מס' משרה</th>
-                          <th><GeoFill /> איזור</th>
-                          <th><Hammer /> תפקיד</th>
-                          <th><Percent /> אחוז משרה</th>
-                          <th><PeopleFill /> מועמדים שניגשו</th>
-                      </tr>
-                  </thead>
-                  <tbody>
+        </div>
 
 
-                      {Object.keys(data).filter((item_key) => {
 
-                          return search.toLowerCase() === '' ? item_key :
-                              filter.toLowerCase() === 'תפקיד' ?
-                                  data[item_key].Job_Name.toLowerCase().includes(search) :
-                                  filter.toLowerCase() === 'איזור' ?
-                                      data[item_key].Job_Location.toLowerCase().includes(search) :
-                                      filter.toLowerCase() === 'אחוז משרה' ?
-                                          data[item_key].Job_Percentage.toLowerCase().includes(search) :
-                                          (data[item_key].Job_Name.toLowerCase().includes(search) || data[item_key].Job_Location.toLowerCase().includes(search) || data[item_key].Job_Percentage.toLowerCase().includes(search));
-
-                      }).sort((a, b) => {
-                          if (sortOrder.toLowerCase() === 'אחוז משרה') {
-                              return data[a].Job_Percentage > data[b].Job_Percentage ? 1 : -1
-                          }
-                          else if (sortOrder.toLowerCase() === 'איזור') {
-                              return data[a].Job_Location > data[b].Job_Location ? 1 : -1
-                          }
-                          else if (sortOrder.toLowerCase() === 'תפקיד') {
-                              return data[a].Job_Name > data[b].Job_Name ? 1 : -1
-                          }
-                          else return 0;
-                      }).map((item_key, index) => {
-
-                          return (
-                              <JobRow key={item_key} data={data} index={index} item_key={item_key}></JobRow>
-                          );
-                      })}
-
-                  </tbody>
-              </table>
-
-          </Container>
+        <table dir='rtl' style={{ width: '100%' }}>
+          <thead>
+            <tr>
+              <th></th>
+              <th><LayoutTextSidebar /> מס' משרה</th>
+              <th><GeoFill /> איזור</th>
+              <th><Hammer /> תפקיד</th>
+              <th><Percent /> אחוז משרה</th>
+              <th><PeopleFill /> מועמדים שניגשו</th>
+            </tr>
+          </thead>
+          <tbody>
 
 
-      </>
+            {Object.keys(data).filter((item_key) => {
+
+              return search.toLowerCase() === '' ? item_key :
+                filter.toLowerCase() === 'תפקיד' ?
+                  data[item_key].Job_Name.toLowerCase().includes(search) :
+                  filter.toLowerCase() === 'איזור' ?
+                    data[item_key].Job_Location.toLowerCase().includes(search) :
+                    filter.toLowerCase() === "אחוז משרה" ?
+                     data[item_key].Job_Percentage.toLowerCase().includes(search) :
+                     (data[item_key].Job_Name.toLowerCase().includes(search) || data[item_key].Job_Location.toLowerCase().includes(search) || data[item_key].Job_Percentage.toLowerCase().includes(search));
+
+            }).sort((a, b) => {
+              if (sortOrder.toLowerCase() === 'אחוז משרה') {
+                return data[a].Job_Percentage > data[b].Job_Percentage ? 1 : -1
+              }
+              else if (sortOrder.toLowerCase() === 'איזור') {
+                return data[a].Job_Location > data[b].Job_Location ? 1 : -1
+              }
+              else if (sortOrder.toLowerCase() === 'תפקיד') {
+                return data[a].Job_Name > data[b].Job_Name ? 1 : -1
+              }
+              else return 0;
+            }).map((item_key, index) => {
+
+              return (
+                <JobRow key={item_key} data={data} index={index} item_key={item_key}></JobRow>
+              );
+            })}
+
+          </tbody>
+        </table>
+
+      </Container>
+
+
+    </>
   );
 };
 
