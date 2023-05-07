55f30bdf8a2ad369cb0ff067030e4de2d7b4db14
aviv0404 <aviv040400@example.com>
2023-05-07 20:47:50 +0300

    same as previous commit

--- src/RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/ViewCandidatesPage.tsx
+++ src/RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/ViewCandidatesPage.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Button, Grid, Container, Typography } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
+import { editButtonStyle, nameStyle, mainGridStyle } from './ViewCandidatesPageStyle';
 
 export default function ViewCandidatesPage() {
+	//TODO: replace this with real info
+	let candidateName = "ישראל ישראלי"
 	return (
-		<Container>
-			
-		</Container>
+		<>
+			<Container sx={mainGridStyle}>
+				<Grid direction={'column'} sx={mainGridStyle} container spacing={2}>
+
+					{/* Edit Button */}
+					<Button sx={editButtonStyle} variant="contained" endIcon={<EditIcon />}>
+						ערוך פרטים
+					</Button>
+
+					{/* Candidate Name */}
+					<Typography sx={nameStyle}>
+						שם: {candidateName}
+					</Typography>
+
+					{/* Candidate Info Table */}
+					<Typography sx={nameStyle}>
+						:משרות
+					</Typography>
+				</Grid>
+			</Container>
+		</>
 	)
 }
---
+++ src/RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/ViewCandidatesPageStyle.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { SxProps } from '@mui/material';
+
+const editButtonStyle: SxProps = {
+    backgroundColor: 'green'
+}
+
+const nameStyle: SxProps = {
+
+}
+
+const mainGridStyle: SxProps = {
+    justifyContent:'flex-end',
+    alignItems: 'flex-end',
+}
+
+export {
+    editButtonStyle,
+    nameStyle,
+    mainGridStyle
+}
