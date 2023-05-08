f497292632749621f68561b69fd63ca81511291e
Elya Athln <elyaathlan1998@gmail.com>
2023-05-08 16:29:22 +0300

    add filtering and sorting func to Candidates

--- src/Firebase/FirebaseFunctions/Candidate.tsx
+++ src/Firebase/FirebaseFunctions/Candidate.tsx
@@ -1,35 +1,122 @@
 import { dataref } from "../FirebaseConfig/firebase";
 const database = dataref;
 export class Candidate {
-	public _id: string;
-	public _firstName: string;
-	public _lastName: string;
-	public _phone: string;
-	public _eMail: string;
-	public _generalRating: number;
+    public _id: string;
+    public _firstName: string;
+    public _lastName: string;
+    public _phone: string;
+    public _eMail: string;
+    public _generalRating: number;
 
-	constructor(firstName: string = "", lastName: string = "", phone: string = "", eMail: string = "", generalRating: number = -1) {
-		this._id = eMail + phone;
-		this._firstName = firstName;
-		this._lastName = lastName;
-		this._phone = phone;
-		this._eMail = eMail;
-		this._generalRating = generalRating;
-	}
+    constructor(firstName: string = "", lastName: string = "", phone: string = "", eMail: string = "", generalRating: number = -1) {
+        this._id = eMail + phone;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._phone = phone;
+        this._eMail = eMail;
+        this._generalRating = generalRating;
+    }
 }
 async function getCandidatesFromDatabase(): Promise<Candidate[]> {
-	const database = dataref;
-	try {
-		const snapshot = await database.ref("/Candidates").once("value");
-		const candidatesData = snapshot.val();
-		const candidates: Candidate[] = [];
-		for (const candidateId in candidatesData) {
-			const candidate = candidatesData[candidateId];
-			candidates.push(candidate);
-		}
-		return candidates;
-	} catch (error) {
-		console.error(error);
-		throw new Error("Failed to fetch candidates from database.");
-	}
+    const database = dataref;
+    try {
+        const snapshot = await database.ref("/Candidates").once("value");
+        const candidatesData = snapshot.val();
+        const candidates: Candidate[] = [];
+        for (const candidateId in candidatesData) {
+            const candidate = candidatesData[candidateId];
+            candidates.push(candidate);
+        }
+        return candidates;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to fetch candidates from database.");
+    }
+}
+export async function getFilteredCandidates(attributes: string[] = [], values: string[] = [], sortBy: string = "") {
+    if (attributes.length !== values.length) {
+        console.log("the attributes length not match to values length")
+        return [];
+    }
+    let candidates = await getCandidatesFromDatabase();
+    //filtering
+    let i = attributes.indexOf("id");
+    if (i >= 0) {
+        candidates = candidates.filter(candidate => candidate._id === values[i])
+    }
+    i = attributes.indexOf("firstName");
+    if (i >= 0) {
+        candidates = candidates.filter(candidate => candidate._firstName === values[i])
+    }
+    i = attributes.indexOf("lastName");
+    if (i >= 0) {
+        candidates = candidates.filter(candidate => candidate._lastName === values[i])
+    }
+    i = attributes.indexOf("phone");
+    if (i >= 0) {
+        candidates = candidates.filter(candidate => candidate._phone === values[i])
+    }
+    i = attributes.indexOf("eMail");
+    if (i >= 0) {
+        candidates = candidates.filter(candidate => candidate._eMail === values[i])
+    }
+    i = attributes.indexOf("generalRating");
+    if (i >= 0) {
+        candidates = candidates.filter(candidate => candidate._generalRating.toString() === values[i])
+    }
+    if (sortBy === 'firstName')
+        return candidates.sort(sortByFirstName);
+    if (sortBy === 'lastName')
+        return candidates.sort(sortByLastName);
+    if (sortBy === 'phone')
+        return candidates.sort(sortByPhone);
+    if (sortBy === 'eMail')
+        return candidates.sort(sortByEmail);
+    if (sortBy === 'generalRating')
+        return candidates.sort(sortByGeneralRating);
+    return candidates;
+}
+/* compare function for sort */
+function sortByFirstName(a: Candidate, b: Candidate): number {
+    if (a._firstName.toLowerCase() < b._firstName.toLowerCase()) {
+        return -1;
+    }
+    if (a._firstName.toLowerCase() > b._firstName.toLowerCase()) {
+        return 1;
+    }
+    return 0;
+}
+
+function sortByLastName(a: Candidate, b: Candidate): number {
+    if (a._lastName.toLowerCase() < b._lastName.toLowerCase()) {
+        return -1;
+    }
+    if (a._lastName.toLowerCase() > b._lastName.toLowerCase()) {
+        return 1;
+    }
+    return 0;
+}
+
+function sortByPhone(a: Candidate, b: Candidate): number {
+    if (a._phone < b._phone) {
+        return -1;
+    }
+    if (a._phone > b._phone) {
+        return 1;
+    }
+    return 0;
+}
+
+function sortByEmail(a: Candidate, b: Candidate): number {
+    if (a._eMail.toLowerCase() < b._eMail.toLowerCase()) {
+        return -1;
+    }
+    if (a._eMail.toLowerCase() > b._eMail.toLowerCase()) {
+        return 1;
+    }
+    return 0;
+}
+
+function sortByGeneralRating(a: Candidate, b: Candidate): number {
+    return b._generalRating - a._generalRating;
 }
