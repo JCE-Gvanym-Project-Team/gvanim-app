170fdbba5bfd4823051463cb54ec7b6c56075925
aviv0404 <aviv040400@example.com>
2023-05-08 18:58:42 +0300

    added fix for RTL problems, and added material ui packages

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// RTL - Right to Left imports
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+// take care of RTL stuff
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+const theme = createTheme({
+  direction: 'rtl',
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </CacheProvider>
   </React.StrictMode>
 );
 
