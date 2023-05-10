48efc8e2af6310bd5282690f34f26da6c577020b
Elya Athln <elyaathlan1998@gmail.com>
2023-05-10 11:09:40 +0300

    add login and register functions

--- src/Firebase/FirebaseFunctions/DBfuncs.tsx
+++ src/Firebase/FirebaseFunctions/DBfuncs.tsx
@@ -2,11 +2,12 @@ import firebase from "firebase/app";
 import "firebase/database";
 import { dataref } from "../FirebaseConfig/firebase";
 import { Job, generateJobNumber } from "./Job";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { getFilteredCandidates, Candidate } from "./Candidate";
 import { CandidateJobStatus, getFilteredCandidateJobStatuses } from "./CandidateJobStatus";
 import { Recomendation } from "./Recomendation";
 import { Recruiter } from "./Recruiter";
+import { registerRecruiter, loginRecruiter, loguotRecruiter } from "./Authentification";
+import { Logout } from "@mui/icons-material";
 const database = dataref;
 /**
  * Prints the data located at the given path in the Firebase Realtime Database.
@@ -61,25 +62,13 @@ export async function appendToDatabase(obj: any, path: string) {
 		console.error("Error adding object to database:", error);
 	}
 }
-/**
- * Submits a job application for a candidate with the given information.
- * @param {string} firstName - The first name of the candidate.
- * @param {string} lastName - The last name of the candidate.
- * @param {string} phone - The phone number of the candidate.
- * @param {string} eMail - The email address of the candidate.
- * @param {string} about - A brief description of the candidate's qualifications.
- * @param {number} jobNumber - The job number of the job the candidate is applying for.
- * @returns {Promise<CandidateJobStatus>} - A promise that resolves to the CandidateJobStatus object representing the application.
- * you can use addRecomendation() on returned value to add a new recomendation.
- * If the candidate has already applied to the job,
- */
 export async function getFirebaseIdsAtPath(path: string): Promise<string[]> {
 	const snapshot = await database.ref(path).once("value");
 	const values = snapshot.val();
 	return values ? Object.keys(values) : [];
 }
 export async function main() {	//for debugging dont use
-	let job1 = new Job((await generateJobNumber()),"title1", "role", [50, 100], "tel-aviv", "tel-aviv-meguorim", "desc", "req", true, false);
-	let job2 = new Job((await generateJobNumber()),"title2", "role", [50, 100], "tel-aviv", "tel-aviv-meguorim", "desc", "req", true, false);
-	let rec = new Recruiter()
-}
+	/*
+	let job1 = new Job((await generateJobNumber()), "title1", "role", [50, 100], "tel-aviv", "tel-aviv-meguorim", "desc", "req", true, false);
+	let job2 = new Job((await generateJobNumber()), "title2", "role", [50, 100], "tel-aviv", "tel-aviv-meguorim", "desc", "req", true, false);
+}	*/
