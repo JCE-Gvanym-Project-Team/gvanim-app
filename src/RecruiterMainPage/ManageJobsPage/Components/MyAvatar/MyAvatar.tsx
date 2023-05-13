f106c737b3bc8f0361003c9733a6df9b314ed912
omer10068 <omer10068@gmail.com>
2023-05-13 03:56:04 +0300

    MyAvatar style arrangement

--- src/RecruiterMainPage/ManageJobsPage/Components/MyAvatar/MyAvatar.tsx
+++ src/RecruiterMainPage/ManageJobsPage/Components/MyAvatar/MyAvatar.tsx
@@ -1,18 +1,12 @@
-import * as React from 'react';
 import Badge from '@mui/material/Badge';
 import { Avatar } from '@mui/material';
 import SvgLogo from "../../../../Components/Logo/Logo.svg"
+import { AvatarSx } from './MyAvatarStyle';
 
 export default function MyAvatar() {
     return (
             <Badge color="info" badgeContent="42" anchorOrigin={{ vertical: 'top', horizontal: 'left', }} overlap="circular">
-                <Avatar variant='circular' src={SvgLogo} sx={{ 
-                    border: '1px solid #dee2e6', width: 90, height: 90,
-                    
-                    '& .MuiAvatar-img': {
-                        objectFit: 'contain',
-                    }
-                    }} />
+                <Avatar variant='circular' src={SvgLogo} sx={ AvatarSx } />
             </Badge>
     );
 }
