8838a764134071fc1d1e3e77fcc27392abeaa48e
aviv0404 <aviv040400@example.com>
2023-05-22 11:59:10 +0300

    changes in style

--- src/RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/ViewCandidatesPageStyle.tsx
+++ src/RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/ViewCandidatesPageStyle.tsx
@@ -1,28 +1,21 @@
 import React from 'react'
 import { SxProps } from '@mui/material';
 
-const editButtonStyle: SxProps = {
+export const editButtonStyle: SxProps = {
     backgroundColor: 'green',
     alignSelf: 'end'
 }
 
-const textStyle: SxProps = {
+export const textStyle: SxProps = {
     color: 'black'
 }
 
-const titleStyle: SxProps = {
+export const titleStyle: SxProps = {
     color: 'blue',
     alignSelf: 'center'
 }
 
-const mainStackStyle: SxProps = {
+export const mainStackStyle: SxProps = {
     marginTop: '1rem',
     justifyContent: 'space-between',
-}
-
-export {
-    editButtonStyle,
-    textStyle,
-    titleStyle,
-    mainStackStyle
 }
