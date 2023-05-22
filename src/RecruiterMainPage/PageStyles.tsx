5777393f6c8cfe34bad96c1e97fa4efc3c6a947a
aviv0404 <aviv040400@example.com>
2023-05-22 13:06:45 +0300

    added global page styles

---
+++ src/RecruiterMainPage/PageStyles.tsx
@@ -0,0 +1,20 @@
+export const GlobalStyle = {
+    font: '',
+    NavbarBackgroundColor: 'rgb(52, 71, 103)'
+}
+
+export const ManageJobsPageGlobalStyle = {
+    NewJobButtonColor: "info",
+    PublishJobButton: 'rgb(52, 71, 103)',
+    TitleColor: 'blue',
+    TableBorder: '1px solid black'
+}
+
+export const ManageCandidatesPageGlobalStyle = {
+    MyTextColorExample: 'green',
+}
+
+export const ReportsPageGlobalStyle = {
+    ReportsBorderSize: '20px',
+    TableBorder: '1px solid black'
+}
