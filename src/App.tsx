88e240e1128c94ea560dd22381822107d4be0999
aviv0404 <aviv040400@gmail.com>
2023-06-03 18:44:49 +0300

    fixed routes completely (maybe)

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,26 @@ import Auth from './RecruiterMainPage/Components/Auth/Auth';
 import DrushimMainPage from './DrushimMainPage/DrushimMainPage';
 import { main } from './Firebase/FirebaseFunctions/test';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { ColorModeContext, useMode } from './DrushimMainPage/theme';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, Switch } from '@mui/material';
+import WelcomePage from './RecruiterMainPage/WelcomePage/WelcomePage';
+import SingleJob from './DrushimMainPage/Components/Job';
+import ManageJobsPage from './RecruiterMainPage/ManageJobsPage/ManageJobsPage';
+import NewJobPage from './RecruiterMainPage/ManageJobsPage/Components/NewJobPage/NewJobPage';
+import EditCandidate from './RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/Components/EditCandidate/EditCandidate';
+import ManageCandidatesPage from './RecruiterMainPage/ManageCandidatesPage/ManageCandidatesPage';
+import React, { useEffect, useState } from 'react';
+import ViewCandidatesPage from './RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/ViewCandidatesPage';
+import ManageInterviewsPage from './RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/ManageInterViewsPage/ManageInterViewsPage';
+import ReportsPage from './RecruiterMainPage/ReportsPage/ReportsPage';
+import AdminPage from './RecruiterMainPage/Components/AdminPage/AdminPage';
+import { getFilteredCandidates, getFilteredJobs, Job } from './Firebase/FirebaseFunctions/functionIndex';
+import NavBar from './RecruiterMainPage/Components/NavBar/NavBar';
+import Footer from './RecruiterMainPage/Components/Footer/Footer';
+import AllJobsPage from './DrushimMainPage/AllJobsPage/AllJobsPage';
+import OneJobPage from './DrushimMainPage/OneJobPage/OneJobPage';
+import RecruiterMainPage from './RecruiterMainPage/RecruiterMainPage';
 
 main();
 const recruitersPageTheme = createTheme({
@@ -13,29 +30,78 @@ const recruitersPageTheme = createTheme({
 
 function App()
 {
-	// for drushim page theme
+
+	// ------------------- FOR RECRUITERS PAGE -------------------
+	const [allJobs, setAllJobs] = React.useState<any[]>([]);
+	const [candidateIDs, setCandidateIDs] = useState<string[]>([])
+
+
+	const fetchAllJobs = async () =>
+	{
+		const jobs = await getFilteredJobs();
+		const jobsWithId = jobs.map((job) => ({ ...job, id: job._jobNumber }));
+		setAllJobs(jobsWithId);
+	};
+
+	const fetchCandidateIDs = async () =>
+	{
+		const candidates = await getFilteredCandidates();
+		setCandidateIDs(candidates.map(candidate => candidate._id));
+	}
+
+	useEffect(() =>
+	{
+		fetchAllJobs();
+		fetchCandidateIDs();
+	}, []);
+
+	const handlelogout = () =>
+	{
+
+	}
+	// ------------------- FOR RECRUITERS PAGE -------------------
+
+
+
+	// ------------------- FOR DRUSHIM PAGE -------------------
 	const temp = useMode();
 	const drushimPageTheme = temp[0];
 	const colorMode = temp[1];
 
+	const [jobs, setJobs] = useState<Job[]>([]);
+
+	const fetchJobs = async () =>
+	{
+		setJobs(await getFilteredJobs());
+	}
+
+	useEffect(() =>
+	{
+		fetchJobs();
+	}, [])
+	// ------------------- FOR DRUSHIM PAGE -------------------
+
 	return (
 		<>
-			{/* Recruiters page */}
-			<ThemeProvider theme={recruitersPageTheme}>
-				<BrowserRouter>
-					<Auth />
-				</BrowserRouter>
-			</ThemeProvider>
+			<Routes>
+				{/* Recruiters page */}
+				<Route path='/management/*' element={
+					<ThemeProvider theme={recruitersPageTheme}>
+						<Auth />
+					</ThemeProvider>
+				}
+				/>
 
-			{/* Drushim Page */}
-			<ColorModeContext.Provider value={colorMode}>
-				<ThemeProvider theme={drushimPageTheme}>
-					<BrowserRouter>
-						<CssBaseline />
-						<DrushimMainPage />
-					</BrowserRouter>
-				</ThemeProvider>
-			</ColorModeContext.Provider>
+				{/* Drushim Page */}
+				<Route path='/career/*' element={
+					<ColorModeContext.Provider value={colorMode} >
+						<ThemeProvider theme={drushimPageTheme}>
+							<DrushimMainPage />
+						</ThemeProvider>
+					</ColorModeContext.Provider >
+				}
+				/>
+			</Routes>
 		</>
 	);
 }
