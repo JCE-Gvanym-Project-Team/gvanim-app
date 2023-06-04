c3e64c081d179b9e1760ada3ca92715778614b0d
Gavriel <Gavrish299@gmail.com>
2023-06-04 19:05:42 +0300

    CandidatePage

---
+++ src/RecruiterMainPage/ManageCandidatesPage/CandidateTable/CandidateTableStyle.tsx
@@ -0,0 +1,88 @@
+import { SxProps, createTheme } from '@mui/material';
+import { Theme } from '@mui/material/styles';
+
+
+export const MyButtonSx: SxProps = {
+		backgroundColor: 'rgb(52, 71, 103)',
+		":hover": {
+			bgcolor: "rgb(52, 71, 103)",
+		}
+}
+export const dataGridContainerSx: SxProps = {
+	overflow: 'hidden',
+	border: '1px solid rgba(0, 0, 0, 0.125)',
+	borderRadius: '1.5rem'
+}
+
+export const dataGridContainerStyle = {
+	padding: 0,
+	marginTop: '15px',
+	marginBottom: '15px',
+	maxWidth: "100%",
+}
+
+export const dataGridSx = (theme: Theme): SxProps => ({
+	padding: 1,
+	height: 'calc(100vh - 112px)',	
+	overflow: 'hidden',
+	boxShadow: 2,
+	"&.MuiDataGrid-root .MuiDataGrid-cell:focus-within": {
+		outline: "none !important",
+
+	},
+	border: 0,
+	color:
+		theme.palette.mode === 'light' ? 'rgba(0,0,0,.85)' : 'rgba(255,255,255,0.85)',
+	fontFamily: [
+		'-apple-system',
+		'BlinkMacSystemFont',
+		'"Segoe UI"',
+		'Roboto',
+		'"Helvetica Neue"',
+		'Arial',
+		'sans-serif',
+		'"Apple Color Emoji"',
+		'"Segoe UI Emoji"',
+		'"Segoe UI Symbol"',
+	].join(','),
+	WebkitFontSmoothing: 'auto',
+	letterSpacing: 'normal',
+	'& .MuiDataGrid-columnsContainer': {
+		backgroundColor: theme.palette.mode === 'light' ? '#fafafa' : '#1d1d1d',
+	},
+	'& .MuiDataGrid-iconSeparator': {
+		display: 'none',
+	},
+	'& .MuiDataGrid-columnHeader': {
+		borderBottom: `1px solid ${theme.palette.mode === 'light' ? '#f0f0f0' : '#303030'}`,
+	},
+	'& .MuiDataGrid-columnHeader, .MuiDataGrid-cell': {
+		borderRight: `1px solid ${theme.palette.mode === 'light' ? '#f0f0f0' : '#303030'}`,
+	},
+	'& .MuiDataGrid-columnsContainer, .MuiDataGrid-cell': {
+		borderBottom: `1px solid ${theme.palette.mode === 'light' ? '#f0f0f0' : '#303030'
+			}`,
+	},
+	'& .MuiDataGrid-cell': {
+		color:
+			theme.palette.mode === 'light' ? 'rgba(0,0,0,.85)' : 'rgba(255,255,255,0.65)',
+	},
+	'& .MuiPaginationItem-root': {
+		borderRadius: 0,
+	}
+});
+
+export const TypographyFooterSx: SxProps = {
+	fontWeight: '500',
+	marginLeft: '8px',
+	color: 'rgb(62, 80, 96)',
+	fontFamily: '"IBM Plex Sans", -apple-system, BlinkMacSystemFont,"Segoe UI", "Roboto", "Helvetica Neue", "Arial", "sans-serif", "Apple Color Emoji","Segoe UI Emoji", "Segoe UI Symbol"'
+}
+
+export const GridFooterContainerSx: SxProps = {
+	width: '100%',
+	display: 'flex',
+	justifyContent: 'center'
+}
+
+
---
+++ src/RecruiterMainPage/ManageCandidatesPage/DropMenu/DropMenu.tsx
@@ -0,0 +1,217 @@
+import * as React from "react";
+import Menu, { MenuActions } from "@mui/base/Menu";
+import MenuItem, { menuItemClasses } from "@mui/base/MenuItem";
+import Popper from "@mui/base/Popper";
+import { styled } from "@mui/system";
+import { ListActionTypes } from "@mui/base/useList";
+import { Box, IconButton, ListItemIcon, Typography } from "@mui/material";
+import { Assignment, Edit, Label, MoreVert, Print } from "@mui/icons-material";
+import {
+  MenuItemIconSx,
+  MenuItemTypographySx,
+  MoreVertSx,
+  blue,
+  grey,
+} from "./DropMenuStyle";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function MenuSection({ children, label }: MenuSectionProps) {
+  return (
+    <MenuSectionRoot>
+      <MenuSectionLabel>{label}</MenuSectionLabel>
+      {children}
+    </MenuSectionRoot>
+  );
+}
+
+export default function MyDropMenu(props: { JobId: any }) {
+  const navigate = useNavigate();
+
+  const { JobId } = props;
+  const [buttonElement, setButtonElement] =
+    React.useState<HTMLButtonElement | null>(null);
+  const [isOpen, setOpen] = React.useState(false);
+  const menuActions = React.useRef<MenuActions>(null);
+  const preventReopen = React.useRef(false);
+
+  const updateAnchor = React.useCallback((node: HTMLButtonElement | null) => {
+    setButtonElement(node);
+  }, []);
+
+  const handleEditClick = () => {
+    setOpen(false);
+    buttonElement?.focus();
+    navigate("/management/createJob", { state: JobId });
+  };
+
+  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (preventReopen.current) {
+      event.preventDefault();
+      preventReopen.current = false;
+      return;
+    }
+
+    setOpen((open) => !open);
+  };
+
+  const handleButtonMouseDown = () => {
+    if (isOpen) {
+      // Prevents the menu from reopening right after closing
+      // when clicking the button.
+      preventReopen.current = true;
+    }
+  };
+
+  const handleButtonKeyDown = (
+    event: React.KeyboardEvent<HTMLButtonElement>
+  ) => {
+    if (event.key === "ArrowDown" || event.key === "ArrowUp") {
+      event.preventDefault();
+      setOpen(true);
+      if (event.key === "ArrowUp") {
+        // Focus the last item when pressing ArrowUp.
+        menuActions.current?.dispatch({
+          type: ListActionTypes.keyDown,
+          key: event.key,
+          event,
+        });
+      }
+    }
+  };
+
+  const createHandleMenuClick = (menuItem: string) => {
+    return () => {
+      console.log(`Clicked on ${menuItem}`);
+      setOpen(false);
+      buttonElement?.focus();
+    };
+  };
+
+  return (
+    <Box>
+      <IconButton
+        type="button"
+        onClick={handleButtonClick}
+        onKeyDown={handleButtonKeyDown}
+        onMouseDown={handleButtonMouseDown}
+        ref={updateAnchor}
+        aria-controls={isOpen ? "wrapped-menu" : undefined}
+        aria-expanded={isOpen || undefined}
+        aria-haspopup="menu"
+      >
+        <MoreVert sx={MoreVertSx} />
+      </IconButton>
+
+      <Menu
+        actions={menuActions}
+        open={isOpen}
+        onOpenChange={(open) => {
+          setOpen(open);
+        }}
+        anchorEl={buttonElement}
+        slots={{ root: StyledPopper, listbox: StyledListbox }}
+        slotProps={{ listbox: { id: "simple-menu" } }}
+      >
+        <Typography
+          variant="caption"
+          sx={{ fontSize: 10, fontWeight: 600, padding: 1 }}
+        >
+          אפשרויות
+        </Typography>
+        <StyledMenuItem onClick={() => navigate(`/career/jobs/${JobId}`)}>
+          <ListItemIcon>
+            <Assignment sx={MenuItemIconSx} />
+
+            <Typography sx={MenuItemTypographySx}>לדף המשרה</Typography>
+          </ListItemIcon>
+        </StyledMenuItem>
+
+        <StyledMenuItem onClick={handleEditClick}>
+          <ListItemIcon>
+            <Edit sx={MenuItemIconSx} />
+            <Typography sx={MenuItemTypographySx}>ערוך מועמד</Typography>
+          </ListItemIcon>
+        </StyledMenuItem>
+
+        {/* </MenuSection> */}
+      </Menu>
+    </Box>
+  );
+}
+
+const StyledListbox = styled("ul")(
+  ({ theme }) => `
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  padding: 6px;
+  margin: 12px 0;
+  min-width: 200px;
+  border-radius: 12px;
+  overflow: auto;
+  outline: 0px;
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  box-shadow: 0px 2px 16px ${
+    theme.palette.mode === "dark" ? grey[900] : grey[200]
+  };
+  `
+);
+
+const StyledMenuItem = styled(MenuItem)(
+  ({ theme }) => `
+  list-style: none;
+  padding: 8px;
+  border-radius: 8px;
+  cursor: default;
+  user-select: none;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+
+  &.${menuItemClasses.focusVisible} {
+    outline: 3px solid ${theme.palette.mode === "dark" ? blue[600] : blue[200]};
+    background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  }
+
+  &.${menuItemClasses.disabled} {
+    color: ${theme.palette.mode === "dark" ? grey[700] : grey[400]};
+  }
+
+  &:hover:not(.${menuItemClasses.disabled}) {
+    background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  }
+  `
+);
+
+const StyledPopper = styled(Popper)`
+  z-index: 1;
+`;
+
+interface MenuSectionProps {
+  children: React.ReactNode;
+  label: string;
+}
+
+const MenuSectionRoot = styled("li")`
+  list-style: none;
+
+  & > ul {
+    padding-left: 1em;
+  }
+`;
+
+const MenuSectionLabel = styled("span")`
+  display: block;
+  padding: 10px 0 5px 10px;
+  font-size: 0.75em;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 0.05rem;
+  color: ${grey[600]};
+`;
