e6b83516385b6215e6505fa72ffeebe68c13a72e
aviv0404 <aviv040400@gmail.com>
2023-06-10 20:53:36 +0300

    changed IsPhoneValid regex

--- src/DrushimMainPage/OneJobPage/OneJobPage.tsx
+++ src/DrushimMainPage/OneJobPage/OneJobPage.tsx
@@ -269,7 +269,7 @@ export default function OneJobPage()
 
     const isPhoneValid = (phone: string) =>
     {
-        return /^(?:(?:(\+?972|\(\+?972\)|\+?\(972\))(?:\s|\.|-)?([1-9]\d?))|(0[23489]{1})|(0[57]{1}[0-9]))(?:\s|\.|-)?([^0\D]{1}\d{2}(?:\s|\.|-)?\d{4})$/gm.test(phone);
+        return /^05[0-57-8][0-9]{7}$/gm.test(phone);
     }
 
     const isEmailValid = (email: string) =>
