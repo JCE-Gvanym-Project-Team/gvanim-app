057eb3b727d0e84ea19abe4fce37ec35aa9c1c9a
omer10068 <omer10068@gmail.com>
2023-06-11 21:22:27 +0300

    handle jobs page

--- src/DrushimMainPage/AllJobsPage/Components/JobItem/JobItem.tsx
+++ src/DrushimMainPage/AllJobsPage/Components/JobItem/JobItem.tsx
@@ -4,6 +4,7 @@ import Paper from '@mui/material/Paper';
 import { Avatar, Box, Chip, Divider, Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { ArticleOutlined, Place } from '@mui/icons-material';
+import { blue } from '@mui/material/colors';
 
 
 
@@ -18,137 +19,129 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function JobItem(props: { job: any }) {
     const { job } = props;
     return (
-        <Item sx={{ 
-            height: '250px', 
-             border: '1px solid rgba(0, 0, 0, 0.095)',
-             borderRadius: '0.475rem' }}>
+        <Item sx={{
+            minHeight: '250px',
+            border: '1px solid rgba(0, 0, 0, 0.095)',
+            borderRadius: '0.475rem'
+        }}>
             <Avatar
                 sx={{
-                    
                     width: '60px',
                     height: '60px',
                     marginY: -5,
                     content: '""',
                     position: 'absolute',
                     zIndex: 0,
-
                     background: 'linear-gradient(to top, rgb(83, 87, 206), rgb(148, 110, 214))',
                     borderRadius: '1rem',
+                }}>
 
-     
-     }}>
-    
 
-    <ArticleOutlined />
+                <ArticleOutlined />
 
 
             </Avatar>
-            <Stack direction='column' justifyContent='space-between' height='100%' >
-                <Box sx={{
-                      borderBottom: '1px solid rgba(0, 0, 0, 0.095)',
-                      top: 0, 
-                      height: '55px' }}>
-             
-                    <Box display='flex' justifyContent='end' id="header">
+            <Box id="CardTopBar" display='flex' justifyContent='end'>
 
-               <Typography sx={{
-                   fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-                   fontSize: '0.8rem',
-                   lineHeight: 1.375,
-                   fontWeight: 500,
-                   opacity: 1,
-                   textTransform: 'none',
-                   verticalAlign: 'unset',
-                   textDecoration: 'none',
-                   color: 'rgb(52, 71, 103)',
-                   letterSpacing: '-0.125px'
-              }}>
-                  משרה מס' {job?._jobNumber} 
-              </Typography>
-               </Box>
-               
-               <Typography sx={{
-                   mt: '0.1rem',
-                   fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-                   fontSize: '1.2rem',
-                   lineHeight: 1.375,
-                   fontWeight: 500,
-                   opacity: 1,
-                   textTransform: 'none',
-                   verticalAlign: 'unset',
-                   textDecoration: 'none',
-                   color: 'rgb(52, 71, 103)',
-                   letterSpacing: '-0.125px'
-              }}>
-                   {job?._title} 
-              </Typography>
+                <Typography sx={{
+                    fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+                    fontSize: '0.8rem',
+                    color: 'rgb(52, 71, 103)',
+                    letterSpacing: '-0.125px'
+                }}>
+                    משרה מס' {job?._jobNumber}
+                </Typography>
+            </Box>
+
+            <Stack direction='column' justifyContent='space-between' height='100%' divider={<Divider />} spacing={1.5} sx={{ mt: 1 }} >
+                <Box id="header">
+
+                    <Box>
+                        <Typography variant='h5' sx={{
+                            fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+                            mt: '0.3rem',
+                            fontWeight: 'bold',
+                            color: 'black',
+
+                        }}>
+                            {job?._title}
+                        </Typography>
                     </Box>
-                    {/* <Divider /> */}
-                <Box display='flex' flexDirection='column' justifyContent='space-between' sx={{
-                    //   border: '1px solid rgba(0, 0, 0, 0.095)',
-                     borderRadius: '0.25rem', top: 0, height: '110px', paddingRight: 1,paddingLeft: 1}} id='body'>
-                  <Box>
-                  {job?._requirements}
-                  </Box>
+                </Box>
 
-                    <Stack display='flex' flexDirection='row' justifyContent='start' sx={{bottom: 0}}>
-<Typography sx={{fontFamily: 'Roboto, Helvetica, Arial, sans-serif',color: 'black',fontWeight: 600 ,alignSelf:'end', mr: 0.5}} fontSize={13}>היקף משרה: </Typography>
-<Typography sx={{fontFamily: 'Roboto, Helvetica, Arial, sans-serif'}} fontSize={13}>
-    50%-70%
-</Typography>
+                <Box id='body' display='flex' flexDirection='column' justifyContent='space-between' sx={{ minHeight: '130px' }}>
+                    <Box>
+                        <Typography variant='h6' sx={{ fontFamily: 'Roboto, Helvetica, Arial, sans-serif', }}>
+                            {job?._requirements}
+                        </Typography>
+                    </Box>
+
+                    <Stack display='flex' flexDirection='row' justifyContent='space-between' sx={{ bottom: 0 }}>
+                        <Box display='flex' flexDirection='row'>
+                            <Typography sx={{ fontFamily: 'Roboto, Helvetica, Arial, sans-serif', color: 'black', fontWeight: 'bold', alignSelf: 'end', mr: 0.5 }} fontSize={13}>היקף משרה: </Typography>
+                            <Typography sx={{ fontFamily: 'Roboto, Helvetica, Arial, sans-serif',alignSelf: 'end' }} fontSize={13}>
+                                {`${job?._scope[0]}%-${job?._scope[1]}%`}
+                            </Typography>
+                        </Box>
+
+                        <Box display='flex' flexDirection='row'>
+                        <Typography sx={{ fontFamily: 'Roboto, Helvetica, Arial, sans-serif', color: 'black', fontWeight: 'bold', alignSelf: 'end', mr: 0.5 }} fontSize={13}>מיקום:</Typography>
+                            <Typography sx={{ fontFamily: 'Roboto, Helvetica, Arial, sans-serif',alignSelf: 'end' }} fontSize={13}>
+                                {job?._region}
+                            </Typography>
+                        </Box>
                     </Stack>
                 </Box>
-                    <Divider />
+
+
                 <Box sx={{
-                    //  border: '1px solid rgba(0, 0, 0, 0.095)', 
-                     top: 0, bottom: 0, display: 'flex', justifyContent: 'space-between' }}>
+
+                    top: 0, bottom: 0, display: 'flex', justifyContent: 'space-between'
+                }}>
 
                     <Box>
                         <Chip variant='outlined' size='small' sx={{
-                            color: '#fff', opacity: 0.72, padding: '0.05rem', fontSize: '0.75rem',
+                            color: '#fff', opacity: 0.72, fontSize: '0.75rem',
                             borderRadius: '8px', backgroundColor: 'rgba(0, 0, 0, 0.38)'
                         }}
                             label={job?._creationDate.toLocaleDateString()} />
                     </Box>
 
-                    <Link to={''} 
-                    style={{
-                        fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-                        fontSize: '1rem',
-                        lineHeight: 1.375,
-                        fontWeight: 600,
-                        opacity: 1,
-                        textTransform: 'none',
-                        verticalAlign: 'unset',
-                        textDecoration: 'none',
-                        color: 'rgb(52, 71, 103)',
-                        letterSpacing: '-0.125px'
-                        // textDecoration: 'unset'
+                    <Link to={`${job?._jobNumber}`}
+                        style={{
+                            fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+                            fontSize: '1rem',
+                            lineHeight: 1.375,
+                            fontWeight: 600,
+                            opacity: 1,
+                            textTransform: 'none',
+                            verticalAlign: 'unset',
+                            textDecoration: 'none',
+                            color: 'rgb(52, 71, 103)',
+                            letterSpacing: '-0.125px'
                         }}>
-                      <Box display='flex' flexDirection='column' justifyContent='end' height='100%'>
-                      <Typography sx={{
-                   
-                             fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-                             fontSize: '1rem',
-                             lineHeight: 1.375,
-                             fontWeight: 600,
-                             opacity: 1,
-                             textTransform: 'none',
-                             verticalAlign: 'unset',
-                             textDecoration: 'none',
-                             color: 'rgb(52, 71, 103)',
-                             letterSpacing: '-0.125px'
-                        }}>
-                            לפרטים 
-                        </Typography>
-                      </Box>
+                        <Box display='flex' flexDirection='column' justifyContent='end'>
+                            <Typography sx={{
+
+                                fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+                                fontSize: '1rem',
+                                lineHeight: 1.375,
+                                fontWeight: 600,
+                                opacity: 1,
+                                textTransform: 'none',
+                                verticalAlign: 'unset',
+                                textDecoration: 'none',
+                                color: 'rgb(52, 71, 103)',
+                                letterSpacing: '-0.125px'
+                            }}>
+                                לפרטים
+                            </Typography>
+                        </Box>
                     </Link>
 
                 </Box>
 
             </Stack>
-
-
         </Item>
     )
 }
