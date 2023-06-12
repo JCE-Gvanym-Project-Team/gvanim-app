9a0baf2e7f84dbc0b0266f3565c073d6a015f439
aviv0404 <aviv040400@gmail.com>
2023-06-12 18:08:04 +0300

    changed snackbar message style

--- src/RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/Components/SuccessMessageSnackbar/SuccessMessageSnackbar.tsx
+++ src/RecruiterMainPage/ManageCandidatesPage/ViewCandidatesPage/Components/SuccessMessageSnackbar/SuccessMessageSnackbar.tsx
@@ -2,39 +2,17 @@ import { Close } from '@mui/icons-material';
 import { Alert, Box, IconButton, Snackbar, Typography } from '@mui/material'
 import React from 'react'
 
-export default function SuccessMessageSnackbar(props: {open, onClose})
+export default function SuccessMessageSnackbar(props: { open, onClose })
 {
-	const {open, onClose} = props;
+	const { open, onClose } = props;
 	return (
 		<Snackbar
 			open={open}
-			onClose={(event, reason) => { onClose(event, reason, false) }}
-			anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-			sx={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}
+			anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+			onClose={onClose}
+			autoHideDuration={6000}
 		>
-			<Alert
-				variant="filled"
-				sx={{ alignItems: 'flex-start' }}
-				action={
-					<React.Fragment>
-						<IconButton
-							aria-label="close"
-							color="inherit"
-							sx={{ p: 0 }}
-							onClick={onClose}
-						>
-							<Close />
-						</IconButton>
-					</React.Fragment>
-				}
-			>
-				<Box sx={{ flex: 1 }}>
-					<Typography variant='h6'>
-						השינויים בוצעו בהצלחה
-					</Typography>
-
-				</Box>
-			</Alert>
+			<Alert children={"השינויים בוצעו בהצלחה"} severity='success' onClose={onClose} />
 		</Snackbar>
 	)
 }
